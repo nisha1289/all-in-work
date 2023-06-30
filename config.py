6e7560ba756080d312a8781aee2ce8234de5426b
RAJPUT <98842578+Shiv1320@users.noreply.github.com>
2023-06-30 13:30:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = ""
-    API_ID = 
-    API_HASH = ""
-    AUTH_USERS = ''
+    BOT_TOKEN = "5060725780:AAEt58lxGTiFEHTmLr2eAuRcJu9MSm3lpLk"
+    API_ID = 15052451
+    API_HASH = "dbf8fdfc66d7a1a9bf359c036409aa14"
+    AUTH_USERS = "1181522124"
 
